ebf7d84891ccb9d9b2062c571561a44e66e2cefd
H. Watanabe <kaityo@users.sourceforge.jp>
2022-05-04 11:53:34 +0900

    initial commit

--- yahoo_news_rss.py
+++ yahoo_news_rss.py
@@ -4,8 +4,10 @@ import datetime
 
 def savexml(topic):
     URL = f"https://news.yahoo.co.jp/rss/topics/{topic}.xml"
+    now = datetime.datetime.now()
+    filename = f"{now:%Y-%m-%d-%H}-{topic}.xml"
     r = requests.get(URL)
-    with open("test.xml", "a") as f:
+    with open(filename, "a") as f:
         f.write(r.text)
 
 
