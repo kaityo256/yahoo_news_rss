74a6d26528fe0d4c1962b89908d950064054aca3
H. Watanabe <kaityo@users.sourceforge.jp>
2022-12-07 19:23:00 +0900

    updates

--- read_xml.py
+++ read_xml.py
@@ -4,8 +4,8 @@ import xml.etree.ElementTree as ET
 def read_xml(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
-    for title in root.find('channel').iterfind('item/title'):
-        print(title.text)
+    #for title in root.find('channel').iterfind('item/title'):
+    #    print(title.text)
 
 
-read_xml("data/2022-09-22-17-top-picks.xml")
+read_xml("data/2022-05-05-13-domestic.xml")
